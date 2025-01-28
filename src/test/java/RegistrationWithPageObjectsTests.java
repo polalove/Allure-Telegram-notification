e8c63dc274801c96c7ac21b84b80750d5cb1663e
Ivan Main PC <pola2288@yandex.ru>
2025-01-28 23:09:22 +0300

    test

---
+++ src/test/java/RegistrationWithPageObjectsTests.java
@@ -0,0 +1,86 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+import pages.components.TableOfDataPage;
+import utils.TestData;
+
+import static io.qameta.allure.Allure.step;
+
+public class RegistrationWithPageObjectsTests extends TestBase {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+    TableOfDataPage tableOfDataPage = new TableOfDataPage();
+    TestData testData = new TestData();
+
+    @Test
+    @DisplayName("Filling out the authorization form")
+    @Tag("simple")
+    void successfulRegistrationTest() {
+        step("Fill form", () -> {
+            registrationPage.openPage()
+                    .setFirstName(testData.firstName)
+                    .setLastName(testData.lastName)
+                    .setEmail(testData.email)
+                    .setGender(testData.gender)
+                    .setUserNumber(testData.mobileNumber)
+                    .setDateOfBirth(testData.dayOfBirth, testData.monthOfBirth, testData.yearOfBirth)
+                    .setSubject(testData.subject)
+                    .setHobbies(testData.hobbies)
+                    .setPicture(testData.picture)
+                    .setAddress(testData.address)
+                    .setStateAndCity(testData.state, testData.city)
+                    .submitPracticeForm();
+        });
+
+        step("Verify results", () -> {
+            tableOfDataPage.checkSuccessPageWithTable();
+            tableOfDataPage.checkDataTable("Student Name", testData.firstName + " " + testData.lastName);
+            tableOfDataPage.checkDataTable("Student Email", testData.email);
+            tableOfDataPage.checkDataTable("Gender", testData.gender);
+            tableOfDataPage.checkDataTable("Mobile", testData.mobileNumber);
+            tableOfDataPage.checkDataTable("Date of Birth", testData.dayOfBirth + " " + testData.monthOfBirth + "," + testData.yearOfBirth);
+            tableOfDataPage.checkDataTable("Subjects", testData.subject);
+            tableOfDataPage.checkDataTable("Hobbies", testData.hobbies);
+            tableOfDataPage.checkDataTable("Picture", testData.picture);
+            tableOfDataPage.checkDataTable("Address", testData.address);
+            tableOfDataPage.checkDataTable("State and City", testData.state + " " + testData.city);
+        });
+    }
+
+    @Test
+    void negativeEmailTest() {
+        registrationPage.openPage()
+                .setFirstName(testData.firstName)
+                .setLastName(testData.lastName)
+                .setEmail(testData.email)
+                .setGender(testData.gender)
+                .setUserNumber(testData.mobileNumber)
+                .setDateOfBirth(testData.dayOfBirth, testData.monthOfBirth, testData.yearOfBirth)
+                .setSubject(testData.subject)
+                .setPicture(testData.picture)
+                .setStateAndCity(testData.state, testData.city)
+                .setHobbies(testData.hobbies)
+                .submitPracticeForm();
+
+        registrationPage.checkUnsuccessPageWithTable();
+    }
+
+    @Test
+    void minDataTest() {
+        registrationPage.openPage()
+                .setFirstName(testData.firstName)
+                .setLastName(testData.lastName)
+                .setGender(testData.gender)
+                .setEmail(testData.email)
+                .setUserNumber(testData.mobileNumber)
+                .submitPracticeForm();
+
+        tableOfDataPage.checkSuccessPageWithTable();
+        tableOfDataPage.checkDataTable("Student Name", testData.firstName + " " + testData.lastName);
+        tableOfDataPage.checkDataTable("Student Email", testData.email);
+        tableOfDataPage.checkDataTable("Gender", testData.gender);
+        tableOfDataPage.checkDataTable("Mobile", testData.mobileNumber);
+
+    }
+}
---
+++ src/test/java/TestBase.java
@@ -0,0 +1,50 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = System.getProperty("browserType");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@" + System.getProperty("remoteHost") +"/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void setupTest() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void teardown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+        Selenide.closeWebDriver();
+    }
+}
+
+//selenoid.autotests.cloud
